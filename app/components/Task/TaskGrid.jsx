8bf9d0a3923876c94f9c21dbf3141561405798c3
Max <2thousandmax@gmail.com>
2022-09-13 23:57:03 +0300

    Task grid

--- app/components/Task/TaskGrid.jsx
+++ app/components/Task/TaskGrid.jsx
@@ -2,7 +2,7 @@ import TaskComponent from "./TaskComponent"
 
 export default ({ tasks }) => {
     return (
-        <div className="w-full grid auto-cols-auto grid-flow-row-dense gap-10 mt-5">
+        <div className="w-full grid grid-cols-3 grid-flow-row gap-10 mt-5">
             {tasks.map((task) => (
                 <TaskComponent key={task.id} task={task} />
             ))}
