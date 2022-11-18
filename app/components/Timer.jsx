640d67ff400c9dac7605c40cf75acbd27e66c2a5
Max <2thousandmax@gmail.com>
2022-11-18 10:12:33 +0300

    Rewrite timer

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,4 +1,64 @@
 import * as React from 'react'
+import moment from 'moment-timezone'
+import clsx from 'clsx'
+
+const CountdownTimer = ({ time, className }) => {
+    // let interval = 1000; // 1 second
+
+    // calculate difference between two times
+    let eventTime = moment.tz(time, "Europe/Moscow");
+
+    // based on time set in user's computer time / OS
+    let currentTime = moment.tz();
+
+    // get duration between two times
+    let duration = moment.duration(eventTime.diff(currentTime));
+
+    // Так и не понял почему оно работает
+    const [timer, setTimer] = React.useState(0)
+
+    React.useEffect(() => {
+        const interval = setInterval(function () {
+            duration = moment.duration(duration - 1000, 'milliseconds')
+            setTimer(duration)
+        }, 1000)
+        return () => clearInterval(interval)
+    }, []);
+
+    return (
+        <div className={clsx('', className)}>
+            <p className='text-2xl mb-5 whitespace-nowrap'>Starts in</p>
+            <div className='flex flex-row flex-nowrap items-center'>
+
+                <div className="w-16 h-16 flex flex-col items-center justify-center border-2 border-gray-500 rounded-lg opacity-80 px-2">
+                    <p className="text-2xl">{duration.days()}</p>
+                    <span className=" text-xs pb-1">days</span>
+                </div>
+                <div className='px-3'>:</div>
+
+                <div className="w-16 h-16 flex flex-col items-center justify-center border-2 border-gray-500 rounded-lg opacity-80 px-2">
+                    <p className="text-2xl">{duration.hours()}</p>
+                    <span className=" text-xs pb-1">hours</span>
+                </div>
+                <div className='px-3'>:</div>
+
+                <div className="w-16 h-16 flex flex-col items-center justify-center border-2 border-gray-500 rounded-lg opacity-80 px-2">
+                    <p className="text-2xl">{duration.minutes()}</p>
+                    <span className=" text-xs pb-1">minutes</span>
+                </div>
+                <div className='px-3'>:</div>
+
+                <div className="w-16 h-16 flex flex-col items-center justify-center border-2 border-gray-500 rounded-lg opacity-80 px-2">
+                    <p className="text-2xl">{duration.seconds()}</p>
+                    <span className=" text-xs pb-1">seconds</span>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export { CountdownTimer }
 
 const Timer = ({ start, finish }) => {
     const [days, setDays] = React.useState(0)
@@ -45,7 +105,7 @@ const Timer = ({ start, finish }) => {
 
     return (
         <>
-            <div className="h-full py-2 flex items-center justify-center space-x-3">
+            <div className="h-full py-2 flex items-center justify-center">
                 {isStarted ? <p>Time to end:</p> : <p>Time to start:</p>}
                 {result}
             </div>
@@ -54,3 +114,5 @@ const Timer = ({ start, finish }) => {
 };
 
 export default Timer;
+
+
