6ac65779d8aaa7c7b9e27e81f61d3f54befb1070
Max <2thousandmax@gmail.com>
2022-11-18 22:08:55 +0300

    Add some custom tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,49 @@
 module.exports = {
-  mode: 'jit',
-  content: [
-    './public/**/*.html',
-    './app/**/*.{js,jsx,ts,tsx,vue}',
-  ],
-  safelist: [
-    'bg-yellow-500',
-    'bg-lime-500',
-    'bg-fuchsia-500',
-    'bg-blue-500',
-    'bg-rose-500',
-    'bg-red-500',
-    'bg-violet-500',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'error': 'hsla(10, 50%, 50%, 0.1)',
-        'error-dark': '#FF0000'
-      },
-      animation: {
-        'spin-slow': 'spin 30s linear infinite',
-      },
-      fontSize:{
-        custom:['8px','8px'],
-      }
+    mode: process.env.NODE_ENV ? 'jit' : undefined,
+    darkMode: 'class',
+    content: [
+        './public/**/*.html',
+        './app/**/*.{js,jsx,ts,tsx,vue}',
+    ],
+    safelist: [
+        'bg-yellow-500',
+        'bg-lime-500',
+        'bg-fuchsia-500',
+        'bg-blue-500',
+        'bg-rose-500',
+        'bg-red-500',
+        'bg-violet-500',
+    ],
+    theme: {
+        // colors: {
+        //     // 'black': '#0c0717',
+        //     // 'black':'#161719',
+        //     // 'grey-100':'rgb(238, 239, 252)',
+        // },
+        extend: {
+            backgroundImage: {
+                'pixel-man': "url('/images/pixel-man.jpg')",
+            },
+            willChange: {
+                'transform-opacity': 'transform, opacity',
+            },
+            colors: {
+                'grey2': 'rgb(238, 239, 252)',
+                'error': 'hsla(10, 50%, 50%, 0.1)',
+                'error-dark': '#FF0000'
+            },
+            animation: {
+                'spin-slow': 'spin 30s linear infinite',
+            },
+            fontSize: {
+                custom: ['8px', '8px'],
+            },
+            height:{
+                '8vh':'80vh',
+            }
+        },
     },
-  },
-  plugins: [
-    require('@shrutibalasa/tailwind-grid-auto-fit'),
-  ],
+    plugins: [
+        require('@shrutibalasa/tailwind-grid-auto-fit'),
+    ],
 }
