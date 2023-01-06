ccf455d90d8c38ecad742920a188fdddb36832d4
Max <2thousandmax@gmail.com>
2023-01-06 15:18:27 +0300

    Add styles to react markdown

--- app/routes/tasks/$taskId/index.jsx
+++ app/routes/tasks/$taskId/index.jsx
@@ -1,14 +1,15 @@
+import * as React from 'react'
 import { json } from '@remix-run/node';
 import { Link, useLoaderData, useParams, useCatch } from '@remix-run/react'
 import clsx from 'clsx'
-import ReactMarkdown from 'react-markdown'
 import { Role } from '@prisma/client'
+import ReactMarkdown from 'react-markdown'
 
 import prisma from '~/utils/prisma.server';
 import authenticator from "~/utils/auth.server";
 
 import { TaskSolutions, TaskFlagInput, TaskHeader, TaskControls } from '~/components/Task'
-import { LinkIcon } from '@heroicons/react/24/outline'
+import { ArrowTopRightOnSquareIcon, LinkIcon } from '@heroicons/react/24/outline'
 
 export async function loader({ request, params }) {
     let player = await authenticator.isAuthenticated(request);
@@ -61,6 +62,9 @@ export async function loader({ request, params }) {
 
         }
 
+        // const taskDescription = await markdownToHtml2(task.content)
+        // console.log(taskDescription)
+
         task = { ...task, solved, solutions }
 
         return json({ task, player })
@@ -72,6 +76,7 @@ export async function loader({ request, params }) {
 
 export default function Task() {
     let { task, player } = useLoaderData();
+
     const tags = task?.labels?.split('-')
     let isAdmin = player?.role == Role.ADMIN
 
@@ -116,14 +121,37 @@ export default function Task() {
                         // h1: ({ children, ...props }) => <h1 className="mt-1 font-bold">{children}</h1>,
                         // Rewrite `em`s (`*like so*`) to `i` with a red foreground color.
                         // em: ({ node, ...props }) => <i className="text-rose-600" {...props} />,
-                        // ul: ({ children, ...props }) => <ul className="list-disc ml-4 mt-4" {...props}>{children}</ul>,
-                        // li: ({ children, ...props }) => <li className="" {...props}>{children}</li>,
-                        a: ({ children, ...props }) => (
-                            <a className='text-rose-600 font-bold flex flex-nowrap items-center not-italic hover:cursor-pointer' style={{ fontStyle: 'normal' }} {...props}>
-                                <p className='hover:underline hover:text-rose-600 not-italic'>{children}</p>
-                                <LinkIcon className='ml-1 w-4 h-4 text-rose-600' />
-                            </a>
-                        ),
+                        ul: function ({ children, ...props }) {
+                            return <ul className="list-disc list-inside" {...props}>{children}</ul>
+                        },
+                        li: function ({ children, ...props }) {
+                            return <li className="ml-3" {...props}>{children}</li>
+                        },
+
+                        p: function ({ className, ...props }) {
+                            console.log(className)
+                            return <p className='py-2' {...props}></p>
+                        },
+                        a: function ({ href, children, ...otherProps }) {
+
+                            console.log(children)
+
+                            return (
+                                <>
+                                    <Link
+                                        to={href}
+                                        className='text-rose-600 font-bold hover:cursor-pointer inline-flex items-center'
+                                        {...otherProps}
+                                    >
+                                        <p className=''>{children}</p>
+                                        <div className='px-1'>
+                                            <ArrowTopRightOnSquareIcon className=' w-4 h-4' />
+                                        </div>
+                                    </Link>
+
+                                </>
+                            )
+                        },
                         // img: ({alt, src}) => (
                         //     <div className="relative w-full aspect-video drop-shadow-xl">
                         //         <Image
@@ -167,10 +195,9 @@ export function CatchBoundary() {
 export function ErrorBoundary({ error }) {
     console.log(error)
     return (
-        <div className=''>
-            <p>Something went wrong</p>
-            <p>We are already working on fixing it</p>
-            <pre>{error.stack}</pre>
+        <div className="h-full p-5 flex flex-col justify-center items-center">
+            <p className="py-2 font-bold text-lg text-center">Something went wrong</p>
+            <p className="font-light text-center">We are already working on fixing it.</p>
         </div>
     );
 }
