fc9b36e648dadff2df356e1d450537e3668603de
rmnvlv <romario2000@bk.ru>
2023-01-27 17:39:44 +0300

    buttons didnt working

--- app/routes/admin/tasks.jsx
+++ app/routes/admin/tasks.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData, Form } from '@remix-run/react'
+import { useLoaderData, Form, useActionData } from '@remix-run/react'
 import { json, redirect } from '@remix-run/node'
 import { TrashIcon } from '@heroicons/react/24/outline'
 import { StarIcon, InformationCircleIcon } from '@heroicons/react/24/solid'
@@ -9,9 +9,71 @@ import { getSession, commitSession } from '~/utils/session.server'
 import { Field } from '~/components/Field'
 import { Button } from '~/components'
 
+export async function loader() {
+    try {
+        let tasks = await prisma.task.findMany()
+
+        return json({ tasks })
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
+}
+
+export async function action({request}) {
+    const formData = await request.formData()
+	let { _action, ...values } = Object.fromEntries(formData)
+
+    switch(_action){
+        case 'edit':{
+            console.log(values.taskId)
+            return redirect("./tasks/" + values.taskId)
+        }
+    }
+}
+
 export default function tasks() {
+    let data = useLoaderData()
+    let actionData = useActionData()
+
     return (
-            <div className="colour-white pt-14"> tasks </div>
+        <div className='container max-w-5xl mx-auto'>
+        <div className='mt-14 px-5 w-full'>
+
+            <div className='py-5 relative overflow-x-auto'>
+                <table className="w-full table-auto">
+                    <thead>
+                        <tr className='h-12 whitespace-nowrap border-b border-white/30 font-bold '>
+                            <td className="px-3">TaskId</td>
+                            <td className="px-3">Name</td>
+                            <td className="px-3 text-center">Description</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.tasks.map((task) => (
+                            <tr key={task.id} className='h-12 whitespace-nowrap border-b  border-white/30 last:border-none'>
+                                <td className="px-3 font-bold">
+                                    <input name='taskId' valur={task.id} type='hidden' />
+                                    {/* {task.id} */}
+                                    <Button 
+                                        name='_action'
+                                        text='Edit'
+                                        value='edit'
+                                    />
+                                </td>
+                                <td className="px-3">
+                                    {task.name}
+                                </td>
+                                <td className="px-3">
+                                    {task.content}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
     )
 }
 
